2e175f8f359089b20e014d7af96bbb63384eb3b1
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-05-17 17:36:46 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    config:function(){
+        return (
+            {
+                adminUsers:[1612452016,1144785510], //admin users' telegram id, should be numbers
+                botToken: '1830621825:AAFZNjljWZKI8MNwoXGNI8KgfocpHB68QA4', // bot token
+
+            }
+        );
+    }
+};
